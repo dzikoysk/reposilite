dfc0a39df579fd78e62093defd78c0e59573c578
dzikoysk <dzikoysk@dzikoysk.net>
2018-10-04 22:36:43 +0200

    Allow to select repository directories from the configuration #2, release 1.0.7

--- src/main/java/org/panda_lang/nanomaven/NanoMavenConstants.java
+++ src/main/java/org/panda_lang/nanomaven/NanoMavenConstants.java
@@ -22,7 +22,7 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public class NanoMavenConstants {
 
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
 
     public static final boolean INTERNAL_DEBUG = true;
 
