30d8f1640cba765365b50e87a0845a6f9e3114f8
dzikoysk <dzikoysk@dzikoysk.net>
2020-05-15 17:59:03 +0200

    Release 2.2.1

--- src/main/java/org/panda_lang/nanomaven/NanoConstants.java
+++ src/main/java/org/panda_lang/nanomaven/NanoConstants.java
@@ -22,7 +22,7 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public final class NanoConstants {
 
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.2.1";
 
     static final String GREETING_MESSAGE = ansi().bold().fg(Color.GREEN).a("NanoMaven ").reset().a(NanoConstants.VERSION).toString();
 
