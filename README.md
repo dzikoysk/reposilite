c30b2a6794f2f7038d28910ebba3d75a4e15689e
dzikoysk <dzikoysk@dzikoysk.net>
2020-05-26 12:55:26 +0200

    GH-44 Fix data files format and release 2.4.2

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ $ docker pull dzikoysk/reposilite
 List of available management commands
 
 ```bash
-Reposilite 2.4.1 Commands:
+Reposilite 2.4.2 Commands:
   help - List available commands
   status - Display metrics
   tokens - List all generated tokens
--- src/main/java/org/panda_lang/reposilite/ReposiliteConstants.java
+++ src/main/java/org/panda_lang/reposilite/ReposiliteConstants.java
@@ -18,7 +18,7 @@ package org.panda_lang.reposilite;
 
 public final class ReposiliteConstants {
 
-    public static final String VERSION = "2.4.1";
+    public static final String VERSION = "2.4.2";
 
     public static final String REMOTE_VERSION = "https://raw.githubusercontent.com/dzikoysk/reposilite/master/version";
 
