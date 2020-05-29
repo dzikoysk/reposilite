d8df817e6d1d48ed360d0e6946b8c55f47d33012
dzikoysk <dzikoysk@dzikoysk.net>
2020-05-29 23:59:03 +0200

    GH-92 Release 2.5.0 (Resolve #92)

---
+++ reposilite-frontend/src/components/FileEntry.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    .file-preview.w-full    
+        a(v-if="file.type === 'file'" :href="getUri()" target="_blank" )
+            FileEntryContent(:file="file")
+        router-link(v-else :to="getUri()")
+            FileEntryContent(:file="file")
+</template>
+
+<script>
+import FileEntryContent from './FileEntryContent'
+
+export default {
+    props: [ 'file' ],
+    components: {
+        FileEntryContent
+    },
+    data: () => ({
+        uri: undefined
+    }),
+    mounted() {
+        this.uri = this.$route.params['qualifier']
+
+        if (this.uri.length > 0) {
+            this.uri += '/'
+        }
+    },
+    methods: {
+        getUri() {
+            return '/' + this.uri + this.file.name
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
