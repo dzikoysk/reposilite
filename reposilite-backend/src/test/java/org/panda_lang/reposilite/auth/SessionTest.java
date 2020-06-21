cb804393286692ac24271652ee930fb4e04aec42
dzikoysk <dzikoysk@dzikoysk.net>
2020-06-21 23:07:24 +0200

    GH-121 Improve the coverage of Session class

--- reposilite-backend/src/test/java/org/panda_lang/reposilite/auth/SessionTest.java
+++ reposilite-backend/src/test/java/org/panda_lang/reposilite/auth/SessionTest.java
@@ -1,13 +1,22 @@
 package org.panda_lang.reposilite.auth;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.panda_lang.reposilite.config.Configuration;
 
 import java.util.Arrays;
 import java.util.Collections;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class SessionTest {
+
+    private static final Configuration DEFAULT_CONFIGURATION = new Configuration();
+
+    static {
+        DEFAULT_CONFIGURATION.setRepositories(Arrays.asList("releases", "snapshots"));
+    }
 
     @Test
     void hasPermission() {
@@ -15,11 +24,11 @@ class SessionTest {
         configuration.setRepositories(Collections.emptyList());
         Session standardSession = new Session(configuration, new Token("/a/b/c", "alias", "token"));
 
-        Assertions.assertTrue(standardSession.hasPermission("/a/b/c"));
-        Assertions.assertTrue(standardSession.hasPermission("/a/b/c/d"));
+        assertTrue(standardSession.hasPermission("/a/b/c"));
+        assertTrue(standardSession.hasPermission("/a/b/c/d"));
 
-        Assertions.assertFalse(standardSession.hasPermission("/a/b/"));
-        Assertions.assertFalse(standardSession.hasPermission("/a/b/d"));
+        assertFalse(standardSession.hasPermission("/a/b/"));
+        assertFalse(standardSession.hasPermission("/a/b/d"));
     }
 
     @Test
@@ -28,26 +37,36 @@ class SessionTest {
         configuration.setRepositories(Arrays.asList("a", "b"));
         Session wildcardSession = new Session(configuration, new Token("*/b/c", "alias", "token"));
 
-        Assertions.assertTrue(wildcardSession.hasPermission("/a/b/c"));
-        Assertions.assertTrue(wildcardSession.hasPermission("/a/b/c/d"));
-        Assertions.assertTrue(wildcardSession.hasPermission("/b/b/c"));
+        assertTrue(wildcardSession.hasPermission("/a/b/c"));
+        assertTrue(wildcardSession.hasPermission("/a/b/c/d"));
+        assertTrue(wildcardSession.hasPermission("/b/b/c"));
 
-        Assertions.assertFalse(wildcardSession.hasPermission("/a/b"));
-        Assertions.assertFalse(wildcardSession.hasPermission("/b/b"));
-        Assertions.assertFalse(wildcardSession.hasPermission("/x/b/c"));
+        assertFalse(wildcardSession.hasPermission("/a/b"));
+        assertFalse(wildcardSession.hasPermission("/b/b"));
+        assertFalse(wildcardSession.hasPermission("/x/b/c"));
     }
 
     @Test
     void hasRootPermission() {
-        Configuration configuration = new Configuration();
-        configuration.setRepositories(Arrays.asList("releases", "snapshots"));
-        Session standardRootSession = new Session(configuration, new Token("/", "alias", "token"));
-        Session wildcardRootSession = new Session(configuration, new Token("*", "alias", "token"));
+        Session standardRootSession = new Session(DEFAULT_CONFIGURATION, new Token("/", "alias", "token"));
+        Session wildcardRootSession = new Session(DEFAULT_CONFIGURATION, new Token("*", "alias", "token"));
 
-        Assertions.assertTrue(standardRootSession.hasPermission("/"));
-        Assertions.assertFalse(wildcardRootSession.hasPermission("/"));
-        Assertions.assertTrue(wildcardRootSession.hasPermission("/releases"));
-        Assertions.assertTrue(wildcardRootSession.hasPermission("/snapshots"));
+        assertTrue(standardRootSession.hasPermission("/"));
+        assertFalse(wildcardRootSession.hasPermission("/"));
+        assertTrue(wildcardRootSession.hasPermission("/releases"));
+        assertTrue(wildcardRootSession.hasPermission("/snapshots"));
+    }
+
+    @Test
+    void shouldContainRepositoryFromPath() {
+        Session session = new Session(DEFAULT_CONFIGURATION, new Token("/releases", "alias", "token"));
+        assertEquals(Collections.singletonList("releases"), session.getRepositories());
+    }
+
+    @Test
+    void shouldReturnEmptyListForUnknownRepositoryInPath() {
+        Session session = new Session(DEFAULT_CONFIGURATION, new Token("/unknown_repository", "alias", "token"));
+        assertEquals(Collections.emptyList(), session.getRepositories());
     }
 
 }
