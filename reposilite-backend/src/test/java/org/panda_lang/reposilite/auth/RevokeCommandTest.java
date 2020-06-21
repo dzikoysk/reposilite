5a358440c27755b2f54ac3695709d9fab9d34475
dzikoysk <dzikoysk@dzikoysk.net>
2020-06-21 23:33:01 +0200

    GH-121 Improve the RevokeCommand error handling

--- reposilite-backend/src/test/java/org/panda_lang/reposilite/auth/RevokeCommandTest.java
+++ reposilite-backend/src/test/java/org/panda_lang/reposilite/auth/RevokeCommandTest.java
@@ -3,6 +3,12 @@ package org.panda_lang.reposilite.auth;
 import org.junit.jupiter.api.Test;
 import org.panda_lang.reposilite.ReposiliteIntegrationTest;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,9 +19,28 @@ class RevokeCommandTest extends ReposiliteIntegrationTest {
         TokenService tokenService = reposilite.getTokenService();
         tokenService.addToken(new Token("path", "alias", "secret"));
 
-        RevokeCommand revokeCommand = new RevokeCommand("alias");
-        assertTrue(revokeCommand.execute(reposilite));
+        assertTrue(new RevokeCommand("alias").execute(reposilite));
         assertNull(tokenService.getToken("alias"));
+    }
+
+    @Test
+    void shouldFalseIfTokenDoesNotExist() {
+        assertFalse(new RevokeCommand("unknown_token").execute(super.reposilite));
+    }
+
+    @Test
+    void shouldFalseIfFileIsNotAvailable() throws Exception {
+        super.reposilite.getTokenService().addToken(new Token("path", "alias", "secret"));
+
+        File tokensFile = new File(super.workingDirectory, "tokens.yml");
+        RandomAccessFile randomAccessFile = new RandomAccessFile(tokensFile, "rw");
+        FileChannel channel = randomAccessFile.getChannel();
+        FileLock lock = channel.lock();
+
+        assertFalse(new RevokeCommand("alias").execute(reposilite));
+        lock.release();
+        channel.close();
+        assertTrue(new RevokeCommand("alias").execute(reposilite));
     }
 
 }
