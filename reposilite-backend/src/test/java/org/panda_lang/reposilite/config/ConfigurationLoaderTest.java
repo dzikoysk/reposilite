4f679b54e9a1233c07f8dced6ca4a64e20ac1579
dzikoysk <dzikoysk@dzikoysk.net>
2020-06-27 23:58:37 +0200

    GH-121 Fix random test issues caused by the  messed up proxied property

--- reposilite-backend/src/test/java/org/panda_lang/reposilite/config/ConfigurationLoaderTest.java
+++ reposilite-backend/src/test/java/org/panda_lang/reposilite/config/ConfigurationLoaderTest.java
@@ -17,26 +17,30 @@ class ConfigurationLoaderTest {
 
     @Test
     void shouldLoadFileWithCustomProperties() {
-        System.setProperty("reposilite.hostname", "localhost");         // String type
-        System.setProperty("reposilite.port", "8080");                  // Integer type
-        System.setProperty("reposilite.debugEnabled", "true");          // Boolean type
-        System.setProperty("reposilite.proxied", "http://a.com,b.com"); // List<String> type
-        System.setProperty("reposilite.repositories", " ");              // Skip empty
+        try {
+            System.setProperty("reposilite.hostname", "localhost");         // String type
+            System.setProperty("reposilite.port", "8080");                  // Integer type
+            System.setProperty("reposilite.debugEnabled", "true");          // Boolean type
+            System.setProperty("reposilite.proxied", "http://a.com,b.com"); // List<String> type
+            System.setProperty("reposilite.repositories", " ");              // Skip empty
+
+            Configuration configuration = ConfigurationLoader.load(workingDirectory.getAbsolutePath());
+            assertEquals("localhost", configuration.getHostname());
+            assertEquals(8080, configuration.getPort());
+            assertTrue(configuration.isDebugEnabled());
+            assertEquals(Arrays.asList("http://a.com", "b.com"), configuration.getProxied());
+            assertFalse(configuration.getRepositories().isEmpty());
+        }
+        finally {
+            // Clean up the system properties to avoid loading of these values by the further tests
+            System.clearProperty("reposilite.hostname");
+            System.clearProperty("reposilite.port");
+            System.clearProperty("reposilite.debugEnabled");
+            System.clearProperty("reposilite.proxied");
+            System.clearProperty("reposilite.repositories");
+        }
 
         Configuration configuration = ConfigurationLoader.load(workingDirectory.getAbsolutePath());
-        assertEquals("localhost", configuration.getHostname());
-        assertEquals(8080, configuration.getPort());
-        assertTrue(configuration.isDebugEnabled());
-        assertEquals(Arrays.asList("http://a.com", "b.com"), configuration.getProxied());
-        assertFalse(configuration.getRepositories().isEmpty());
-
-        // Clean up the system properties to avoid loading of these values by the further tests
-        System.clearProperty("reposilite.hostname");
-        System.clearProperty("reposilite.port");
-        System.clearProperty("reposilite.debugEnabled");
-        System.clearProperty("reposilite.repositories");
-
-        configuration = ConfigurationLoader.load(workingDirectory.getAbsolutePath());
         assertEquals(80, configuration.getPort());
     }
 
