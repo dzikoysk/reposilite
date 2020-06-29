de09a1c021e25565a620f080083658a2c73f7f39
dzikoysk <dzikoysk@dzikoysk.net>
2020-06-29 20:55:56 +0200

    GH-121 Simplify deploy controller test

--- reposilite-backend/src/test/java/org/panda_lang/reposilite/repository/DeployControllerTest.java
+++ reposilite-backend/src/test/java/org/panda_lang/reposilite/repository/DeployControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.panda_lang.reposilite.ReposiliteIntegrationTest;
 import org.panda_lang.utilities.commons.IOUtils;
+import org.panda_lang.utilities.commons.StringUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -33,38 +34,53 @@ class DeployControllerTest extends ReposiliteIntegrationTest {
     @Test
     void shouldReturn401AndArtifactDeploymentIsDisabledMessage() throws IOException, AuthenticationException {
         super.reposilite.getConfiguration().setDeployEnabled(false);
-
-        HttpResponse response = put("/releases/groupId/artifactId/file", new UsernamePasswordCredentials("authtest", "secure"), "content");
-        assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
-
-        String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
-        assertNotNull(result);
-        assertTrue(result.contains("Artifact deployment is disabled"));
+        shouldReturn401AndGivenMessage("/releases/groupId/artifactId/file", "authtest", "secure", "content", "Artifact deployment is disabled");
     }
 
     @Test
     void shouldReturn401AndInvalidCredentialsMessage() throws IOException, AuthenticationException {
-        HttpResponse response = put("/releases/groupId/artifactId/file", new UsernamePasswordCredentials("authtest", "invalid_token"), "content");
-        assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
-
-        String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
-        assertNotNull(result);
-        assertTrue(result.contains("Invalid authorization credentials"));
+        shouldReturn401AndGivenMessage("/releases/groupId/artifactId/file", "authtest", "invalid_token", "content", "Invalid authorization credentials");
     }
 
     private void shouldReturn401AndGivenMessage(String uri, String username, String password, String content, String message) throws IOException, AuthenticationException {
-        HttpResponse response = put(uri, new UsernamePasswordCredentials(username, password), content);
+        HttpResponse response = put(uri, username, password, content);
         assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
 
         String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
         assertNotNull(result);
-        assertTrue(result.contains("Invalid authorization credentials"));
+        assertTrue(result.contains(message));
     }
 
-    private HttpResponse put(String uri, UsernamePasswordCredentials credentials, String content) throws IOException, AuthenticationException {
+    @Test
+    void shouldReturn200AndSuccessMessageForMetadataFiles() throws IOException, AuthenticationException {
+        shouldReturn200AndSuccessMessage("/releases/auth/test/maven-metadata.xml", "authtest", "secure", StringUtils.EMPTY);
+    }
+
+    @Test
+    void shouldReturn200AndSuccessMessage() throws IOException, AuthenticationException {
+        shouldReturn200AndSuccessMessage("/releases/auth/test/pom.xml", "authtest", "secure", "maven metadata content");
+    }
+
+    private void shouldReturn200AndSuccessMessage(String uri, String username, String password, String content) throws IOException, AuthenticationException {
+        HttpResponse deployResponse = put(uri, username, password, content);
+        assertEquals(HttpStatus.SC_OK, deployResponse.getStatusLine().getStatusCode());
+
+        String result = IOUtils.toString(deployResponse.getEntity().getContent(), StandardCharsets.UTF_8);
+        assertNotNull(result);
+        assertEquals("Success", result);
+
+        if (StringUtils.isEmpty(content)) {
+            return;
+        }
+
+        assertEquals(HttpStatus.SC_OK, super.getAuthenticated(uri, username, password).getStatusCode());
+        assertEquals(content, super.getAuthenticated(uri, username, password).parseAsString());
+    }
+
+    private HttpResponse put(String uri, String username, String password, String content) throws IOException, AuthenticationException {
         HttpPut httpPut = new HttpPut(url(uri).toString());
         httpPut.setEntity(new StringEntity(content));
-        httpPut.addHeader(new BasicScheme().authenticate(credentials, httpPut, null));
+        httpPut.addHeader(new BasicScheme().authenticate(new UsernamePasswordCredentials(username, password), httpPut, null));
         return client.execute(httpPut);
     }
 
