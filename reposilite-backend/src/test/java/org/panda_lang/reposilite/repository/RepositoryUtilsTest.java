957f5b008bc0896239da273a87f4b40bcd387932
dzikoysk <dzikoysk@dzikoysk.net>
2020-07-12 16:14:01 +0200

    Update copyright

--- reposilite-backend/src/test/java/org/panda_lang/reposilite/repository/RepositoryUtilsTest.java
+++ reposilite-backend/src/test/java/org/panda_lang/reposilite/repository/RepositoryUtilsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.panda_lang.reposilite.repository;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +28,7 @@ class RepositoryUtilsTest {
 
     @Test
     void shouldNotInterfere() {
-        assertEquals("releases/without/repo/", RepositoryUtils.normalizeUri(CONFIGURATION, "releases/without/repo/"));
+        assertEquals("releases/without/repo-one/", RepositoryUtils.normalizeUri(CONFIGURATION, "releases/without/repo-one/"));
     }
 
     @Test
@@ -24,6 +40,7 @@ class RepositoryUtilsTest {
     void shouldNotAllowPathEscapes() {
         assertEquals(StringUtils.EMPTY, RepositoryUtils.normalizeUri(CONFIGURATION, "~/home"));
         assertEquals(StringUtils.EMPTY, RepositoryUtils.normalizeUri(CONFIGURATION, "../../../../monkas"));
+        assertEquals(StringUtils.EMPTY, RepositoryUtils.normalizeUri(CONFIGURATION, "C:\\"));
     }
 
     @Test
