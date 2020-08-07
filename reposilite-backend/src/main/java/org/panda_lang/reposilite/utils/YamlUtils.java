cccd6688a5a6d2940c594c411d80a17e23cda36b
dzikoysk <dzikoysk@dzikoysk.net>
2020-08-08 01:33:07 +0200

    Add missing test cases to ConfigurationLoaderTest

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/YamlUtils.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/YamlUtils.java
@@ -16,7 +16,6 @@
 
 package org.panda_lang.reposilite.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.panda_lang.utilities.commons.FileUtils;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
@@ -24,8 +23,6 @@ import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.function.Function;
 
 public final class YamlUtils {
 
@@ -35,23 +32,8 @@ public final class YamlUtils {
     }};
 
     private static final Yaml YAML = new Yaml(REPRESENTER);
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private YamlUtils() { }
-
-    public static <T> T forceLoad(File file, Class<T> type, Function<Map<String, Object>, Map<String, Object>> patches) {
-        try {
-            return load(file, type, patches);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot load requested file", e);
-        }
-    }
-
-    public static <T> T load(File file, Class<T> type, Function<Map<String, Object>, Map<String, Object>> patches) throws IOException {
-        Map<String, Object> content = YAML.load(FileUtils.getContentOfFile(file));
-        content = patches.apply(content);
-        return OBJECT_MAPPER.convertValue(content, type);
-    }
 
     public static <T> T load(File file, Class<T> type) throws IOException {
         return YAML.loadAs(FileUtils.getContentOfFile(file), type);
