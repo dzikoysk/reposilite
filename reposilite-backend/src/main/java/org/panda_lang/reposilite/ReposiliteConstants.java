9821bb36abfe3d0750369b29bf95a9f90ddee938
dzikoysk <dzikoysk@dzikoysk.net>
2021-02-12 20:50:32 +0100

    Release 2.9.13

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/ReposiliteConstants.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/ReposiliteConstants.java
@@ -18,7 +18,7 @@ package org.panda_lang.reposilite;
 
 public final class ReposiliteConstants {
 
-    public static final String VERSION = "2.9.12";
+    public static final String VERSION = "2.9.13";
 
     public static final String REMOTE_VERSION = "https://repo.panda-lang.org/org/panda-lang/reposilite/latest";
 
