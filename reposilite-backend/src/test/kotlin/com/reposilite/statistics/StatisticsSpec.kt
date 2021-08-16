c74c8287495cd1aae276b36db9b5fa3f0e2c4758
dzikoysk <dzikoysk@dzikoysk.net>
2021-08-17 00:16:45 +0200

    GH-550 Remove deprecated metadata classes and resolve #550

--- reposilite-backend/src/test/kotlin/com/reposilite/statistics/StatisticsSpec.kt
+++ reposilite-backend/src/test/kotlin/com/reposilite/statistics/StatisticsSpec.kt
@@ -1,12 +1,12 @@
 package com.reposilite.statistics
 
-import net.dzikoysk.dynamiclogger.backend.InMemoryLogger
 import com.reposilite.statistics.api.RecordType
 import com.reposilite.statistics.infrastructure.InMemoryStatisticsRepository
+import net.dzikoysk.dynamiclogger.backend.InMemoryLogger
 
 internal open class StatisticsSpec {
 
-    protected val logger = InMemoryLogger()
+    private val logger = InMemoryLogger()
     protected val statisticsFacade = StatisticsFacade(logger, InMemoryStatisticsRepository())
 
     protected fun increaseAndSave(type: RecordType, identifier: String) {
