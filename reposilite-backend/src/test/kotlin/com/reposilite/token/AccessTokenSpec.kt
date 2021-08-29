46646422ddbfc61030c06fe82c37d0fd8fdb4823
dzikoysk <dzikoysk@dzikoysk.net>
2021-08-29 21:13:50 +0200

    GH-477 Fix AccessTokeFacade test

--- reposilite-backend/src/test/kotlin/com/reposilite/token/AccessTokenSpec.kt
+++ reposilite-backend/src/test/kotlin/com/reposilite/token/AccessTokenSpec.kt
@@ -1,11 +1,18 @@
 package com.reposilite.token
 
-import net.dzikoysk.dynamiclogger.backend.InMemoryLogger
+import com.reposilite.token.api.AccessToken
+import com.reposilite.token.api.CreateAccessTokenRequest
+import com.reposilite.token.api.CreateAccessTokenResponse
 import com.reposilite.token.infrastructure.InMemoryAccessTokenRepository
 
 internal open class AccessTokenSpec {
 
-    protected val logger = InMemoryLogger()
-    protected val accessTokenFacade = AccessTokenFacade(logger, InMemoryAccessTokenRepository())
+    protected val accessTokenFacade = AccessTokenFacade(InMemoryAccessTokenRepository(), InMemoryAccessTokenRepository())
+
+    protected fun createToken(name: String): CreateAccessTokenResponse =
+        accessTokenFacade.createAccessToken(CreateAccessTokenRequest(name))
+
+    protected fun createToken(name: String, secret: String): AccessToken =
+        accessTokenFacade.createAccessToken(CreateAccessTokenRequest(name, secret)).accessToken
 
 }
