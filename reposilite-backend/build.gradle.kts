bd254299f219f9878f303abf72a9e8ba36238050
dzikoysk <dzikoysk@dzikoysk.net>
2021-10-03 23:09:29 +0200

    GH-747 Mark sources as 3.0.0-alpha.2 (Resolve #746)

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -20,7 +20,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.panda-lang"
-version = "3.0.0-alpha.1-SNAPSHOT"
+version = "3.0.0-alpha.2"
 
 plugins {
     `java-library`
