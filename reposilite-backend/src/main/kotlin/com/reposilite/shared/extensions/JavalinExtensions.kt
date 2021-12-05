dec6e2f5edc3a45682c18de48a12378f363c7002
dzikoysk <dzikoysk@dzikoysk.net>
2021-12-05 17:06:00 +0100

    GH-765 Move Context#result out of 'use' scope

--- reposilite-backend/src/main/kotlin/com/reposilite/shared/extensions/JavalinExtensions.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/shared/extensions/JavalinExtensions.kt
@@ -3,6 +3,7 @@ package com.reposilite.shared.extensions
 import com.reposilite.web.http.acceptsBody
 import com.reposilite.web.http.contentDisposition
 import com.reposilite.web.http.contentLength
+import com.reposilite.web.silentClose
 import io.javalin.http.ContentType
 import io.javalin.http.Context
 import java.io.InputStream
@@ -18,12 +19,19 @@ internal fun Context.resultAttachment(name: String, contentType: ContentType, co
         contentDisposition(""""attachment; filename="$name" """)
     }
 
+    if (acceptsBody()) {
+        result(data)
+    } else {
+        data.silentClose()
+    }
+
+    /*
     data.use {
         if (acceptsBody()) {
-            result(data)
-            // it.copyTo(output())
+            it.copyTo(output())
         }
     }
+     */
 
     return this
 }
