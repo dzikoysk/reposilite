15f9161aac8ea3b86ac1b39442cc475504912ab6
dzikoysk <dzikoysk@dzikoysk.net>
2022-01-05 22:29:09 +0100

    Manually create database file

--- reposilite-backend/src/main/kotlin/com/reposilite/ReposiliteFactory.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/ReposiliteFactory.kt
@@ -44,6 +44,7 @@ object ReposiliteFactory {
         journalist.logger.info("--- Environment")
         journalist.logger.info("Platform: ${System.getProperty("java.version")} (${System.getProperty("os.name")} :: ${System.getProperty("os.arch")})")
         journalist.logger.info("Working directory: ${parameters.workingDirectory.toAbsolutePath()}")
+        journalist.logger.info("Configuration: ${parameters.localConfigurationFile}")
         journalist.logger.info("Threads: ${localConfiguration.webThreadPool.get()} WEB / ${localConfiguration.ioThreadPool.get()} IO")
         if (parameters.testEnv) journalist.logger.info("Test environment enabled")
 
