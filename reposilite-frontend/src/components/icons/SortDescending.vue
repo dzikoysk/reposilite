83b0380faac298db91ad137774d9903a79a41171
dzikoysk <dzikoysk@dzikoysk.net>
2022-01-11 23:49:49 +0100

    GH-1051 Implement adjustments modal, some cosmetic updates

---
+++ reposilite-frontend/src/components/icons/SortDescending.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Source: https://heroicons.dev/ -->
+  <svg 
+    class="w-6 h-6" 
+    fill="none" 
+    stroke="currentColor" 
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      stroke-linecap="round" 
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
+    />
+  </svg>
+</template>
