f22340dafbf01bce05a4b1f7cae44721a24ea108
dzikoysk <dzikoysk@dzikoysk.net>
2022-02-10 21:29:41 +0100

    GH-1092 Use HikariCP to manage underlying JDBC thread pool per each db target (Fix #1092)

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed")
     api("net.dzikoysk:exposed-upsert:1.0.3")
+    // Threadpool
+    implementation("com.zaxxer:HikariCP:4.0.3")
     // Drivers
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
     implementation("mysql:mysql-connector-java:8.0.28")
