c4513ead3f138d908e242682b0c419fbc7ecc3f9
Dzikoysk <dzikoysk@dzikoysk.net>
2022-02-10 21:54:42 +0100

    GH-1092 Use HikariCP to manage underlying JDBC thread pool per each db target (Fix #1092) (#1099)

--- reposilite-backend/src/main/kotlin/com/reposilite/settings/application/DatabaseSourceFactory.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/settings/application/DatabaseSourceFactory.kt
@@ -19,54 +19,57 @@ package com.reposilite.settings.application
 import com.reposilite.settings.api.LocalConfiguration.StandardSQLDatabaseSettings
 import com.reposilite.settings.api.LocalConfiguration.EmbeddedSQLDatabaseSettings
 import com.reposilite.shared.extensions.loadCommandBasedConfiguration
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import java.io.File
 import java.nio.file.Path
 import java.sql.Connection.TRANSACTION_SERIALIZABLE
+import javax.sql.DataSource
 import kotlin.io.path.absolutePathString
 
 internal object DatabaseSourceFactory {
 
     fun createConnection(workingDirectory: Path, databaseConfiguration: String): Database =
         when {
-            databaseConfiguration.startsWith("mysql") -> {
-                connectWithCredentials(databaseConfiguration, "jdbc:mysql", "com.mysql.cj.jdbc.Driver")
-            }
-            databaseConfiguration.startsWith("sqlite") -> {
-                val settings = loadCommandBasedConfiguration(EmbeddedSQLDatabaseSettings(), databaseConfiguration).configuration
-
-                val database =
-                    if (settings.temporary) {
-                        val temporaryDatabase = File.createTempFile("reposilite-database", ".db")
-                        temporaryDatabase.deleteOnExit()
-                        Database.connect("jdbc:sqlite:${temporaryDatabase.absolutePath}", "org.sqlite.JDBC")
-                    } else {
-                        Database.connect("jdbc:sqlite:${workingDirectory.resolve(settings.fileName).absolutePathString()}", "org.sqlite.JDBC")
-                    }
-
-                TransactionManager.manager.defaultIsolationLevel = TRANSACTION_SERIALIZABLE
-                database
-            }
-            /* Experimental implementations */
-            databaseConfiguration.startsWith("postgresql") -> {
-                connectWithCredentials(databaseConfiguration, "jdbc:postgresql", "org.postgresql.Driver")
-            }
-            databaseConfiguration.startsWith("h2") -> {
-                val settings = loadCommandBasedConfiguration(EmbeddedSQLDatabaseSettings(), databaseConfiguration).configuration
-                Database.connect("jdbc:h2:${workingDirectory.resolve(settings.fileName).absolutePathString()}", "org.h2.Driver")
-            }
+            databaseConfiguration.startsWith("mysql") -> connectWithStandardDatabase(databaseConfiguration, "jdbc:mysql", "com.mysql.cj.jdbc.Driver")
+            databaseConfiguration.startsWith("sqlite") -> connectWithEmbeddedDatabase(workingDirectory, databaseConfiguration, "org.sqlite.JDBC", "jdbc:sqlite:%file%")
+            /* Experimental implementations (not covered with integration tests) */
+            databaseConfiguration.startsWith("postgresql") -> connectWithStandardDatabase(databaseConfiguration, "jdbc:postgresql", "org.postgresql.Driver")
+            databaseConfiguration.startsWith("h2") -> connectWithEmbeddedDatabase(workingDirectory, databaseConfiguration, "org.h2.Driver", "jdbc:h2:%file%")
             else -> throw RuntimeException("Unknown database: $databaseConfiguration")
         }
 
-    private fun connectWithCredentials(databaseConfiguration: String, dialect: String, driver:String): Database =
+    private fun connectWithStandardDatabase(databaseConfiguration: String, dialect: String, driver: String): Database =
         with(loadCommandBasedConfiguration(StandardSQLDatabaseSettings(), databaseConfiguration).configuration) {
-            Database.connect(
-                url = "$dialect://${host}/${database}",
-                driver = driver,
-                user = user,
-                password = password
-            )
+            Database.connect(createDataSource(driver, "$dialect://${host}/${database}", 2, user, password))
         }
+
+    private fun connectWithEmbeddedDatabase(workingDirectory: Path, databaseConfiguration: String, driver: String, dialect: String): Database =
+        with(loadCommandBasedConfiguration(EmbeddedSQLDatabaseSettings(), databaseConfiguration).configuration) {
+            val databaseFile =
+                if (temporary)
+                    File.createTempFile("reposilite-database", ".db")
+                        .also { it.deleteOnExit() }
+                        .toPath()
+                else
+                    workingDirectory.resolve(fileName)
+
+            Database.connect(createDataSource(driver, dialect.replace("%file%", databaseFile.absolutePathString()), 1)).also {
+                TransactionManager.manager.defaultIsolationLevel = TRANSACTION_SERIALIZABLE
+            }
+        }
+
+    private fun createDataSource(driver: String, url: String, threadPool: Int, username: String? = null, password: String? = null): DataSource =
+        HikariDataSource(
+            HikariConfig().apply {
+                this.jdbcUrl = url
+                this.driverClassName = driver
+                this.maximumPoolSize = threadPool
+                username?.also { this.username = it }
+                password?.also { this.password = it }
+            }
+        )
 
 }
