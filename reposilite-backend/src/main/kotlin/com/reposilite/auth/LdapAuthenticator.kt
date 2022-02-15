0aebaf5b035f4160ab234e17a51dc552c504af73
dzikoysk <dzikoysk@dzikoysk.net>
2022-02-15 14:23:13 +0100

    GH-924 Implement LdapAuthenticator#search

--- reposilite-backend/src/main/kotlin/com/reposilite/auth/LdapAuthenticator.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/auth/LdapAuthenticator.kt
@@ -4,15 +4,24 @@ import com.reposilite.auth.api.AuthenticationRequest
 import com.reposilite.settings.api.SharedConfiguration.LdapConfiguration
 import com.reposilite.token.api.AccessTokenDto
 import com.reposilite.web.http.ErrorResponse
+import com.reposilite.web.http.errorResponse
+import com.reposilite.web.http.notFoundError
+import io.javalin.http.HttpCode.BAD_REQUEST
+import io.javalin.http.HttpCode.UNAUTHORIZED
 import panda.std.Result
+import panda.std.asSuccess
 import java.util.Hashtable
 import javax.naming.Context.INITIAL_CONTEXT_FACTORY
 import javax.naming.Context.PROVIDER_URL
 import javax.naming.Context.SECURITY_AUTHENTICATION
 import javax.naming.Context.SECURITY_CREDENTIALS
 import javax.naming.Context.SECURITY_PRINCIPAL
+import javax.naming.NameNotFoundException
 import javax.naming.directory.DirContext
 import javax.naming.directory.InitialDirContext
+import javax.naming.directory.InvalidSearchFilterException
+import javax.naming.directory.SearchControls
+
 
 internal class LdapAuthenticator(
     private val ldapConfiguration: LdapConfiguration
@@ -22,25 +31,67 @@ internal class LdapAuthenticator(
         TODO("Not yet implemented")
     }
 
-    fun search(query: String) {
-        val searchContext = with(ldapConfiguration) {
+    fun search(ldapFilterQuery: String, vararg requestedAttributes: String): Result<List<Map<String, List<String>>>, ErrorResponse> {
+        return with(ldapConfiguration) {
             createContext(hostname, port, searchUserDn, searchUserPassword)
-        }
+        }.flatMap { context ->
+            try {
+                val searchControls = SearchControls().also {
+                    it.returningAttributes = requestedAttributes
+                    it.searchScope = SearchControls.SUBTREE_SCOPE
+                }
 
-        searchContext.close()
+                val searchResults = context.search(ldapConfiguration.baseDn, ldapFilterQuery, searchControls);
+                val result = mutableListOf<Map<String, List<String>>>()
+
+                if (searchResults.hasMore()) {
+                    val resultEntry = searchResults.next()
+                    val attributes = resultEntry.attributes
+                    val mappedAttributes = mutableMapOf<String, MutableList<String>>()
+
+                    requestedAttributes.forEach { attribute ->
+                        mappedAttributes[attribute] = attributes.get(attribute).all.let {
+                            val list = mutableListOf<String>()
+
+                            while (it.hasMore()) {
+                                val next=  it.next()
+                                println(next)
+                                list.add(next.toString())
+                            }
+
+                            it.close()
+                            list
+                        }
+                    }
+
+                    result.add(mappedAttributes)
+                }
+
+                if (result.isEmpty())
+                    notFoundError("Entries not found")
+                else
+                    result.asSuccess()
+            } catch (nameNotFoundException: NameNotFoundException) {
+                notFoundError(nameNotFoundException.message ?: nameNotFoundException.javaClass.toString())
+            } catch (invalidSearchFilterException: InvalidSearchFilterException) {
+                errorResponse(BAD_REQUEST, invalidSearchFilterException.message ?: invalidSearchFilterException.javaClass.toString())
+            }
+        }
     }
 
-    private fun createContext(hostname: String, port: Int, user: String, password: String): DirContext = InitialDirContext(
-        Hashtable<String, String>().also {
-            it[INITIAL_CONTEXT_FACTORY] = "com.sun.jndi.ldap.LdapCtxFactory"
-            it[PROVIDER_URL] = "ldap://$hostname:$port"
-            it[SECURITY_AUTHENTICATION] = "simple"
-            it[SECURITY_PRINCIPAL] = user
-            it[SECURITY_CREDENTIALS] = password
-        }
-    )
+    private fun createContext(hostname: String, port: Int, user: String, password: String): Result<out DirContext, ErrorResponse> =
+        Hashtable<String, String>()
+            .also {
+                it[INITIAL_CONTEXT_FACTORY] = "com.sun.jndi.ldap.LdapCtxFactory"
+                it[PROVIDER_URL] = "ldap://$hostname:$port"
+                it[SECURITY_AUTHENTICATION] = "simple"
+                it[SECURITY_PRINCIPAL] = user
+                it[SECURITY_CREDENTIALS] = password
+            }
+            .let { Result.attempt { InitialDirContext(it) } }
+            .mapErr { ErrorResponse(UNAUTHORIZED, "Unauthorized LDAP access") }
 
     override fun name(): String =
-        "LDAP"
+            "LDAP"
 
 }
--- reposilite-backend/src/test/kotlin/com/reposilite/auth/LdapAuthenticatorTest.kt
+++ reposilite-backend/src/test/kotlin/com/reposilite/auth/LdapAuthenticatorTest.kt
@@ -1,5 +1,6 @@
 package com.reposilite.auth
 
+import com.reposilite.assertCollectionsEquals
 import com.reposilite.settings.api.SharedConfiguration.LdapConfiguration
 import com.unboundid.ldap.listener.InMemoryDirectoryServer
 import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig
@@ -7,6 +8,7 @@ import com.unboundid.ldap.listener.InMemoryListenerConfig
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import panda.std.ResultAssertions.assertOk
 import java.net.InetAddress
 
 class LdapAuthenticatorTest {
@@ -18,18 +20,24 @@ class LdapAuthenticatorTest {
     fun createLdapServer() {
         this.ldapConfiguration = LdapConfiguration().also {
             it.hostname = "ldap.domain.com"
-            it.port = (1024 + (Math.random() * (Short.MAX_VALUE - 1025))).toInt()
+            it.port = (1024 + Math.random() * (Short.MAX_VALUE - 1025)).toInt()
             it.baseDn = "dc=domain,dc=com"
-            it.searchUserDn = "cn=Reposilite,ou=Search Accounts,${it.baseDn}"
+            it.searchUserDn = "cn=Reposilite,ou=Search Accounts,dc=domain,dc=com"
             it.searchUserPassword = "search-secret"
         }
 
-        val config = InMemoryDirectoryServerConfig("dc=ldap,${ldapConfiguration.baseDn}")
+        val config = InMemoryDirectoryServerConfig(ldapConfiguration.baseDn)
         config.addAdditionalBindCredentials(ldapConfiguration.searchUserDn, ldapConfiguration.searchUserPassword)
+        config.addAdditionalBindCredentials("cn=dzikoysk,ou=Reposilite Users,${ldapConfiguration.baseDn}", "secret")
         config.listenerConfigs.add(InMemoryListenerConfig.createLDAPConfig(ldapConfiguration.hostname, InetAddress.getLoopbackAddress(), ldapConfiguration.port, null))
+        config.schema = null // remove
 
         this.ldapServer = InMemoryDirectoryServer(config)
         ldapServer.startListening(ldapConfiguration.hostname)
+        ldapServer.add("dn: dc=domain,dc=com", "objectClass: top", "objectClass: domain")
+        ldapServer.add("dn: ou=Search Accounts,dc=domain,dc=com", "objectClass: organizationalUnit")
+        ldapServer.add("dn: cn=Reposilite,ou=Search Accounts,dc=domain,dc=com", "objectClass: person", "memberOf: ou=Search Accounts,dc=domain,dc=com")
+
         ldapConfiguration.hostname = ldapServer.getListenAddress(ldapConfiguration.hostname).hostAddress
     }
 
@@ -41,7 +49,14 @@ class LdapAuthenticatorTest {
     @Test
     fun `should connect with search user`() {
         val authenticator = LdapAuthenticator(ldapConfiguration)
-        authenticator.search("(&(objectClass=person)(cn=Reposilite))")
+        val result = assertOk(authenticator.search("(&(objectClass=person)(cn=Reposilite))", "cn"))
+
+        assertCollectionsEquals(
+            listOf(
+                mapOf("cn" to listOf(("Reposilite")))
+            ),
+            result
+        )
     }
 
 }
