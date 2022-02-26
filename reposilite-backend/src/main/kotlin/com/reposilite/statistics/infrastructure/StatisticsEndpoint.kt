018e04dd8181630ac32242b4450f232da02ae2d8
dzikoysk <dzikoysk@dzikoysk.net>
2022-02-26 21:15:39 +0100

    GH-1135 Add experimental test mode and fix incomplete 'GROUP BY' queries in PostgreSQL scenarios (Fix #1135)

--- reposilite-backend/src/main/kotlin/com/reposilite/statistics/infrastructure/StatisticsEndpoint.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/statistics/infrastructure/StatisticsEndpoint.kt
@@ -21,29 +21,52 @@ import com.reposilite.statistics.StatisticsFacade
 import com.reposilite.statistics.api.ResolvedCountResponse
 import com.reposilite.web.api.ReposiliteRoute
 import com.reposilite.web.api.ReposiliteRoutes
+import com.reposilite.web.http.ErrorResponse
 import com.reposilite.web.routing.RouteMethod.GET
 import io.javalin.openapi.HttpMethod
 import io.javalin.openapi.OpenApi
+import io.javalin.openapi.OpenApiContent
 import io.javalin.openapi.OpenApiParam
+import io.javalin.openapi.OpenApiResponse
+import panda.std.asSuccess
 
 internal class StatisticsEndpoint(private val statisticsFacade: StatisticsFacade) : ReposiliteRoutes() {
 
     @OpenApi(
-        tags = ["Maven"],
-        path = "/api/statistics/resolved/{limit}/{repository}/*",
+        tags = ["Statistics"],
+        path = "/api/statistics/resolved/phrase/{limit}/{repository}/*",
         methods = [HttpMethod.GET],
         pathParams = [
             OpenApiParam(name = "limit", description = "Amount of records to find (Maximum: $MAX_PAGE_SIZE", required = true),
             OpenApiParam(name = "repository", description = "Repository to search in", required = true),
             OpenApiParam(name = "*", description = "Phrase to search for", required = true, allowEmptyValue = true)
         ],
+        responses = [
+            OpenApiResponse("200", content = [ OpenApiContent(from = ResolvedCountResponse::class) ], description = "Aggregated sum of resolved requests with list a list of them all"),
+            OpenApiResponse("401", content = [ OpenApiContent(from = ErrorResponse::class) ], description = "When invalid token is used")
+        ]
     )
-    val findCount = ReposiliteRoute<ResolvedCountResponse>("/api/statistics/resolved/{limit}/{repository}/<gav>", GET) {
+    val findCountByPhrase = ReposiliteRoute<ResolvedCountResponse>("/api/statistics/resolved/phrase/{limit}/{repository}/<gav>", GET) {
         authorized("/${requireParameter("repository")}/${requireParameter("gav")}") {
             response = statisticsFacade.findResolvedRequestsByPhrase(requireParameter("repository"), requireParameter("gav"), 1)
         }
     }
 
-    override val routes = routes(findCount)
+    @OpenApi(
+        tags = ["Statistics"],
+        path = "/api/statistics/resolved/unique",
+        methods = [HttpMethod.GET],
+        responses = [
+            OpenApiResponse("200", content = [ OpenApiContent(from = Long::class) ], description = "Number of all unique requests"),
+            OpenApiResponse("401", content = [ OpenApiContent(from = ErrorResponse::class) ], description = "When non-manager token is used")
+        ]
+    )
+    val findUniqueCount = ReposiliteRoute<Long>("/api/statistics/resolved/unique", GET) {
+        managerOnly {
+            response = statisticsFacade.countUniqueRecords().asSuccess()
+        }
+    }
+
+    override val routes = routes(findCountByPhrase, findUniqueCount)
 
 }
