497ef7d407dbb2cdd83f9fea65eb94a3f01d13c3
dzikoysk <dzikoysk@dzikoysk.net>
2022-04-07 16:10:37 +0200

    GH-1213 Run H2 in MySQL mode (Fix #1213)

---
+++ reposilite-backend/src/integration/kotlin/com/reposilite/ReposiliteExperimentalRemoteIntegrationJunitExtension.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.reposilite
+
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.Extension
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+/**
+ * Integrations used in remote stack:
+ * - PostreSQL
+ * - Local file system
+ */
+@Testcontainers
+internal class ReposiliteExperimentalRemoteIntegrationJunitExtension : Extension, BeforeEachCallback, AfterEachCallback {
+
+    private class SpecifiedPostgreSQLContainer(image: String) : PostgreSQLContainer<SpecifiedPostgreSQLContainer>(DockerImageName.parse(image))
+
+    @Container
+    private val postgres = SpecifiedPostgreSQLContainer("postgres:13.6")
+
+    override fun beforeEach(context: ExtensionContext?) {
+        postgres.start()
+
+        context?.also {
+            val instance = it.requiredTestInstance
+            val type = instance::class.java
+
+            type.getField("_extensionInitialized").set(instance, true)
+            type.getField("_database").set(instance, "postgresql ${postgres.host}:${postgres.getMappedPort(5432)} ${postgres.databaseName} ${postgres.username} ${postgres.password}")
+            type.getField("_storageProvider").set(instance, "fs")
+        }
+    }
+
+    override fun afterEach(context: ExtensionContext) {
+        postgres.stop()
+    }
+
+}
