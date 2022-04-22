2811d1f767fb0d1df2328abd28d5d9c70e2be225
dzikoysk <dzikoysk@dzikoysk.net>
2022-04-22 02:16:01 +0200

    Release 3.0.0-alpha.25

--- reposilite-backend/src/main/kotlin/com/reposilite/Reposilite.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/Reposilite.kt
@@ -32,7 +32,7 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.atomic.AtomicBoolean
 
-const val VERSION = "3.0.0-alpha.24"
+const val VERSION = "3.0.0-alpha.25"
 
 class Reposilite(
     val journalist: ReposiliteJournalist,
