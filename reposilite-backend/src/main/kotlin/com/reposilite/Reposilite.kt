c94ff7ebf0ae9c86ce1886a10a66404df284e146
dzikoysk <dzikoysk@dzikoysk.net>
2022-05-15 00:27:17 +0200

    Close database before forcing shutdown of other thread pools

--- reposilite-backend/src/main/kotlin/com/reposilite/Reposilite.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/Reposilite.kt
@@ -15,6 +15,8 @@
  */
 package com.reposilite
 
+import com.reposilite.configuration.local.LocalConfiguration
+import com.reposilite.configuration.local.infrastructure.DatabaseConnection
 import com.reposilite.journalist.Journalist
 import com.reposilite.journalist.Logger
 import com.reposilite.plugin.Extensions
@@ -23,8 +25,6 @@ import com.reposilite.plugin.api.ReposiliteDisposeEvent
 import com.reposilite.plugin.api.ReposiliteInitializeEvent
 import com.reposilite.plugin.api.ReposilitePostInitializeEvent
 import com.reposilite.plugin.api.ReposiliteStartedEvent
-import com.reposilite.configuration.local.LocalConfiguration
-import com.reposilite.configuration.local.infrastructure.DatabaseConnection
 import com.reposilite.web.HttpServer
 import panda.std.Result
 import panda.std.Result.ok
@@ -82,9 +82,9 @@ class Reposilite(
             ioService.shutdown()
             extensions.emitEvent(ReposiliteDisposeEvent(this))
             webServer.stop()
+            databaseConnection.close()
             scheduler.shutdownNow()
             ioService.shutdownNow()
-            databaseConnection.close()
             journalist.shutdown()
         }
 
