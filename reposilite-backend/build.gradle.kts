fc19607018cfff57165dc411d20a0533abd4456a
WithoutAName25 <withoutaname@withoutaname.eu>
2022-05-25 14:08:32 +0200

    GH-1330 Internal version for snapshot builds (#1344)
    * Add current version to sources before compiling
    
    * Change generated source dir from "build/generated/kotlin" to "src/generated/kotlin"

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -15,6 +15,7 @@
  */
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.apache.tools.ant.filters.ReplaceTokens
 
 group = "org.panda-lang"
 
@@ -183,6 +184,21 @@ publishing {
             }
         }
     }
+}
+
+tasks.register<Copy>("generateKotlin") {
+    inputs.property("version", version)
+    from("$projectDir/src/template/kotlin")
+    into("$projectDir/src/generated/kotlin")
+    filter(ReplaceTokens::class, "tokens" to mapOf("version" to version))
+}
+
+tasks.compileKotlin {
+    dependsOn("generateKotlin")
+}
+
+kotlin.sourceSets.main {
+    kotlin.srcDir("$projectDir/src/generated/kotlin")
 }
 
 kapt {
