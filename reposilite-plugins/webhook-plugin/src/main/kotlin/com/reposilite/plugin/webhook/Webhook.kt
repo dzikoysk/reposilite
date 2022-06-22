c418a8c45b3033c63f7114b5a262827f3122c19e
Ethan <git@ethanlibs.co>
2022-06-22 14:20:23 -0400

    not sure if this is the correct way to implement this

---
+++ reposilite-plugins/webhook-plugin/src/main/kotlin/com/reposilite/plugin/webhook/Webhook.kt
@@ -0,0 +1,7 @@
+package com.reposilite.plugin.webhook
+
+import com.reposilite.plugin.api.Event
+
+class Webhook {
+    fun fromEvent(event: Event): Webhook? { return null }
+}
--- reposilite-plugins/webhook-plugin/src/main/kotlin/com/reposilite/plugin/webhook/WebhookPlugin.kt
+++ reposilite-plugins/webhook-plugin/src/main/kotlin/com/reposilite/plugin/webhook/WebhookPlugin.kt
@@ -1,12 +1,18 @@
 package com.reposilite.plugin.webhook
 
+import com.reposilite.maven.api.DeployEvent
 import com.reposilite.plugin.api.Facade
 import com.reposilite.plugin.api.Plugin
 import com.reposilite.plugin.api.ReposilitePlugin
+import com.reposilite.plugin.event
 
 @Plugin(name = "webhook")
 class WebhookPlugin : ReposilitePlugin() {
     override fun initialize(): Facade? {
+        event { event: DeployEvent ->
+            Webhook().fromEvent(event);
+        }
+
         return null
     }
 }
