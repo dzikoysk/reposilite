ef44131074422455fedfbdb8dfd2e85587e597f5
Ethan <git@ethanlibs.co>
2022-06-22 10:18:16 -0400

    oops

--- reposilite-plugins/webhook-plugin/src/main/kotlin/com/reposilite/plugin/webhook/WebhookPlugin.kt
+++ reposilite-plugins/webhook-plugin/src/main/kotlin/com/reposilite/plugin/webhook/WebhookPlugin.kt
@@ -1,7 +1,12 @@
 package com.reposilite.plugin.webhook
 
+import com.reposilite.plugin.api.Facade
 import com.reposilite.plugin.api.Plugin
+import com.reposilite.plugin.api.ReposilitePlugin
 
 @Plugin(name = "webhook")
-class WebhookPlugin {
+class WebhookPlugin : ReposilitePlugin() {
+    override fun initialize(): Facade? {
+        return null
+    }
 }
