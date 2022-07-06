62300d86e1a85b1a638023b37c8fa63291678e68
Joshua Sing <joshua@hypera.dev>
2022-07-06 22:51:03 +1000

    fix: CardMenu dropdown click

--- reposilite-frontend/src/components/card/CodeSnippet.vue
+++ reposilite-frontend/src/components/card/CodeSnippet.vue
@@ -18,8 +18,8 @@
 import {  computed } from 'vue'
 import { PrismEditor } from 'vue-prism-editor'
 import 'vue-prism-editor/dist/prismeditor.min.css'
-import prism from "prismjs"
-import "prismjs/themes/prism-coy.css"
+import prism from 'prismjs'
+import 'prismjs/themes/prism-coy.css'
 
 const props = defineProps({
   configuration: {
