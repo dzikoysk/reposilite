13cbb12c87bcd76edac3842499c942734932455f
dzikoysk <dzikoysk@dzikoysk.net>
2022-10-01 01:58:05 +0200

    Move publication configuration to the main build file

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -148,27 +148,6 @@ tasks.withType<ShadowJar> {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("library") {
-            from(components.getByName("java"))
-            artifactId = "reposilite"
-            // Gradle generator does not support <repositories> section from Maven specification.
-            // ~ https://github.com/gradle/gradle/issues/15932
-            pom.withXml {
-                val repositories = asNode().appendNode("repositories")
-                project.repositories.findAll(closureOf<Any> {
-                    if (this is MavenArtifactRepository && this.url.toString().startsWith("https")) {
-                        val repository = repositories.appendNode("repository")
-                        repository.appendNode("id", this.url.toString().replace("https://", "").replace(".", "-").replace("/", "-"))
-                        repository.appendNode("url", this.url.toString())
-                    }
-                })
-            }
-        }
-    }
-}
-
 tasks.register<Copy>("generateKotlin") {
     inputs.property("version", version)
     from("$projectDir/src/template/kotlin")
--- reposilite-plugins/build.gradle.kts
+++ reposilite-plugins/build.gradle.kts
@@ -1,23 +1,3 @@
 subprojects {
     group = "com.reposilite.plugin"
-
-    publishing {
-        publications {
-            create<MavenPublication>("library") {
-                from(components.getByName("java"))
-                // Gradle generator does not support <repositories> section from Maven specification.
-                // ~ https://github.com/gradle/gradle/issues/15932
-                pom.withXml {
-                    val repositories = asNode().appendNode("repositories")
-                    project.repositories.findAll(closureOf<Any> {
-                        if (this is MavenArtifactRepository && this.url.toString().startsWith("https")) {
-                            val repository = repositories.appendNode("repository")
-                            repository.appendNode("id", this.name)
-                            repository.appendNode("url", this.url.toString())
-                        }
-                    })
-                }
-            }
-        }
-    }
 }
