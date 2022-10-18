419f8257e82232006b7a0073713b23f412fd4ef4
zrdzn <zrdzn9@gmail.com>
2022-10-18 18:54:25 +0200

    Resolve once again (monke moment)

--- reposilite-backend/src/main/kotlin/com/reposilite/configuration/local/LocalConfiguration.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/configuration/local/LocalConfiguration.kt
@@ -86,9 +86,12 @@ class LocalConfiguration : Facade {
 
     @Description("# Key file to use.")
     @Description("# You can specify absolute path to the given file or use \${WORKING_DIRECTORY} variable.")
-    @Description("# If you want to use .pem certificate you need to specify its path next to the key path:")
+    @Description("# If you want to use .pem certificate you need to specify its path next to the key path.")
+    @Description("# Example .pem paths setup:")
     @Description("# keyPath: \${WORKING_DIRECTORY}/key.pem \${WORKING_DIRECTORY}/cert.pem")
-    val keyPath = reference("\${WORKING_DIRECTORY}/keystore.jks")
+    @Description("# Example .jks path setup:")
+    @Description("# keyPath: \${WORKING_DIRECTORY}/keystore.jks")
+    val keyPath = reference("\${WORKING_DIRECTORY}/key.pem \${WORKING_DIRECTORY}/cert.pem")
 
     @Description("# Key password to use")
     val keyPassword = reference("")
