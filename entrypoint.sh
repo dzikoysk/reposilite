9799671362533c2a47899dd59308b3b44161310b
dzikoysk <dzikoysk@dzikoysk.net>
2023-04-04 12:29:25 +0200

    GH-1762 Create dedicated user in container only if it was launched as root (Resolve #1762)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-GROUP_ID="${PGID:-999}"
-addgroup --gid $GROUP_ID reposilite
-
-USER_ID="${PUID:-999}"
-adduser --system -uid $USER_ID --ingroup reposilite --shell /bin/sh reposilite
-
-chown -R reposilite:reposilite /app
-chown -R reposilite:reposilite /var/log/reposilite
-
-REPOSILITE_ARGS=$REPOSILITE_OPTS
+REPOSILITE_ARGS="$REPOSILITE_OPTS"
 case "$REPOSILITE_OPTS" in
   *"--working-directory"*) ;;
   *"-wd"*                ) ;;
   *                      ) REPOSILITE_ARGS="--working-directory=/app/data $REPOSILITE_ARGS";;
 esac
 
-# shellcheck disable=SC2093
-# shellcheck disable=SC2086
-exec runuser -u reposilite -- \
-  java \
-     -Dtinylog.writerFile.file="/var/log/reposilite/log_{date}.txt" \
-     -Dtinylog.writerFile.latest=/var/log/reposilite/latest.log \
-     $JAVA_OPTS \
-     -jar reposilite.jar \
-     $REPOSILITE_ARGS
+if [ "$UID" -ne "0" ]; then
+  exec java \
+       -Dtinylog.writerFile.file="/var/log/reposilite/log_{date}.txt" \
+       -Dtinylog.writerFile.latest=/var/log/reposilite/latest.log \
+       $JAVA_OPTS \
+       -jar reposilite.jar \
+       $REPOSILITE_ARGS
+else
+  GROUP_ID="${PGID:-999}"
+  addgroup --gid "$GROUP_ID" reposilite
+
+  USER_ID="${PUID:-999}"
+  adduser --system -uid "$USER_ID" --ingroup reposilite --shell /bin/sh reposilite
+
+  chown -R reposilite:reposilite /app
+  chown -R reposilite:reposilite /var/log/reposilite
+
+  exec runuser -u reposilite -- \
+    java \
+       -Dtinylog.writerFile.file="/var/log/reposilite/log_{date}.txt" \
+       -Dtinylog.writerFile.latest=/var/log/reposilite/latest.log \
+       $JAVA_OPTS \
+       -jar reposilite.jar \
+       $REPOSILITE_ARGS
+fi
+
