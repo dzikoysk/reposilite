7e6a833e33b5cb1004ac423135d6fc5a1b1be80f
Pétrus Pradella <petrus_ant@hotmail.com>
2023-07-06 06:24:15 -0300

    GH-262 Add an extra message after startup (#1870)
    * Add StartupDone message!
    
    Add message 
    > Startup Done! For help, type "help" or "?"
    After reposilite is up and running.
    
    * Typo
    
    ---------
    
    Co-authored-by: dzikoysk <dzikoysk@dzikoysk.net>

--- reposilite-backend/src/main/kotlin/com/reposilite/console/application/ConsolePlugin.kt
+++ reposilite-backend/src/main/kotlin/com/reposilite/console/application/ConsolePlugin.kt
@@ -84,6 +84,7 @@ internal class ConsolePlugin : ReposilitePlugin() {
                     logger.info("")
                     consoleFacade.executeCommand("status")
                     logger.info("")
+                    logger.info("Startup done! For help, type 'help' or '?'")
                 }
             }
         }
