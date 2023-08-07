3fe7b1049b2ffd8205603f36565d1032231e1803
dzikoysk <dzikoysk@dzikoysk.net>
2023-08-07 23:47:36 +0200

    GH-1899 Downgrade Exposed

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     val awsSdkV1 = "1.12.486"
     testImplementation("com.amazonaws:aws-java-sdk-s3:$awsSdkV1")
 
-    val exposed = "0.42.0"
+    val exposed = "0.41.1"
     api("org.jetbrains.exposed:exposed-core:$exposed")
     api("org.jetbrains.exposed:exposed-dao:$exposed")
     api("org.jetbrains.exposed:exposed-jdbc:$exposed")
