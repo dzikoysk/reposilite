68b73f19dc9811ccf10936430cf17f7b0e622bd6
dzikoysk <dzikoysk@dzikoysk.net>
2024-04-13 15:31:27 +0200

    GH-2098 Use `id -u` instead of `$UID` to verify if current user is root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# shellcheck disable=SC2086
 set -e
 
 REPOSILITE_ARGS="$REPOSILITE_OPTS"
@@ -8,20 +10,22 @@ case "$REPOSILITE_OPTS" in
   *                      ) REPOSILITE_ARGS="--working-directory=/app/data $REPOSILITE_ARGS";;
 esac
 
-if [ "$UID" -ne "0" ]; then
+# GH-1762: support for running as non-root user
+if (($(id -u) != 0)); then
   exec java \
        -Dtinylog.writerFile.file="/var/log/reposilite/log_{date}.txt" \
        -Dtinylog.writerFile.latest=/var/log/reposilite/latest.log \
        $JAVA_OPTS \
        -jar reposilite.jar \
        $REPOSILITE_ARGS
+# GH-1200: run as non-root user
 else
+  # GH-1634: support custom user and group ids
   GROUP_ID="${PGID:-999}"
- if ! grep -q "^reposilite" /etc/group;
+  if ! grep -q "^reposilite" /etc/group;
   then
     addgroup --gid "$GROUP_ID" reposilite;
   fi
-
   USER_ID="${PUID:-999}"
   if ! grep "^reposilite" /etc/passwd;
   then
@@ -39,4 +43,3 @@ else
        -jar reposilite.jar \
        $REPOSILITE_ARGS
 fi
-
