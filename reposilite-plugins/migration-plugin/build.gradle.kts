0183ff0e41c71b17107bab88e867043270f182cd
dzikoysk <dzikoysk@dzikoysk.net>
2024-08-02 20:45:47 +0200

    GH-2195 Fix kaml-jvm version

--- reposilite-plugins/migration-plugin/build.gradle.kts
+++ reposilite-plugins/migration-plugin/build.gradle.kts
@@ -29,7 +29,7 @@ application {
 dependencies {
     compileOnly(project(":reposilite-backend"))
     testImplementation(project(":reposilite-backend"))
-    implementation("com.charleskorn.kaml:kaml-jvm:0.60.1")
+    implementation("com.charleskorn.kaml:kaml-jvm:0.61.0")
 }
 
 tasks.withType<ShadowJar> {
