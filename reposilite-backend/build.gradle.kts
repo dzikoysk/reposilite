b202895952dc684a1df4a6aa4e444d619ede57c4
dzikoysk <dzikoysk@dzikoysk.net>
2025-01-02 21:30:53 +0100

    GH-2327 Exclude new MySQL jdbc artifact from minimize (Fix #2327)

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -164,6 +164,7 @@ tasks.withType<ShadowJar> {
         exclude(dependency("org.xerial:sqlite-jdbc.*"))
         exclude(dependency("org.sqlite:.*"))
         exclude(dependency("mysql:.*"))
+        exclude(dependency("com.mysql:.*"))
         exclude(dependency("org.mariadb.jdbc:.*"))
         exclude(dependency("org.postgresql:.*"))
         exclude(dependency("org.h2:.*"))
