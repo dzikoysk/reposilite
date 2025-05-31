5c7aaaaebe1d82642ba2adfc65ada0f32d421f62
dzikoysk <dzikoysk@dzikoysk.net>
2025-05-31 22:49:49 +0200

    GH-2394 Fix build

--- reposilite-backend/build.gradle.kts
+++ reposilite-backend/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     val bcrypt = "0.10.2"
     implementation("at.favre.lib:bcrypt:$bcrypt")
 
-    val expressible = "1.3.8"
+    val expressible = "1.3.6"
     api("org.panda-lang:expressible:$expressible")
     api("org.panda-lang:expressible-kt:$expressible")
     testImplementation("org.panda-lang:expressible-junit:$expressible")
