f5449acd9fc8a5bfe6fd5d7008afb9ee92227be1
dzikoysk <dzikoysk@dzikoysk.net>
2026-02-28 17:43:13 +0100

    chore: adjust entrypoint.sh startup procedure

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,18 +22,6 @@ if [ "$(id -u)" != 0 ]; then
 # GH-1200: run as non-root user
 else
 
-  # GH-2288: Dockerfile Step-1 migration warning
-  # shellcheck disable=SC2012
-  existing_uid=$(ls -dln data | awk '{print $3}')
-  # shellcheck disable=SC2012
-  existing_gid=$(ls -dln data | awk '{print $4}')
-  if [ $existing_uid != "977" ]||[ $existing_gid != "977" ]; then
-    printf "\033[1;31mStarting with Reposilite 3.5.20 the standard user id will be 977, I will make those changes now.\033[0m\n" 1>&2
-    printf "\033[1;31mAfter 3.6.0 docker installations with user id of 999 will no longer work.\033[0m\n" 1>&2
-    printf "\033[1;31mFor more information see: https://github.com/dzikoysk/reposilite/issues/2288\033[0m\n" 1>&2
-    printf "\033[1;31mIF YOU DOWNGRADE PAST THIS POINT \"Hic sunt dracones\"\033[0m\n" 1>&2
-  fi
-
   # GH-1634: support custom user and group ids
   GROUP_ID="${PGID:-977}"
   if ! grep -q "^reposilite" /etc/group;
@@ -46,8 +34,23 @@ else
     adduser --system -uid "$USER_ID" --ingroup reposilite --shell /bin/sh reposilite;
   fi
 
-  chown -R reposilite:reposilite /app
-  chown -R reposilite:reposilite /var/log/reposilite
+  # GH-2288: Dockerfile Step-1 migration warning
+  # shellcheck disable=SC2012
+  existing_uid=$(ls -dln data | awk '{print $3}')
+  # shellcheck disable=SC2012
+  existing_gid=$(ls -dln data | awk '{print $4}')
+  if [ "$existing_uid" != "$USER_ID" ] || [ "$existing_gid" != "$GROUP_ID" ]; then
+    printf "\033[1;31mStarting with Reposilite 3.5.20 the standard user id will be 977, I will make those changes now.\033[0m\n" 1>&2
+    printf "\033[1;31mAfter 3.6.0 docker installations with user id of 999 will no longer work.\033[0m\n" 1>&2
+    printf "\033[1;31mFor more information see: https://github.com/dzikoysk/reposilite/issues/2288\033[0m\n" 1>&2
+    printf "\033[1;31mIF YOU DOWNGRADE PAST THIS POINT \"Hic sunt dracones\"\033[0m\n" 1>&2
+  fi
+
+  # GH-2457: skip chown if ownership already matches target UID/GID
+  if [ "$REPOSILITE_FORCE_CHOWN" = "true" ] || [ "$existing_uid" != "$USER_ID" ] || [ "$existing_gid" != "$GROUP_ID" ]; then
+    chown -R reposilite:reposilite /app
+    chown -R reposilite:reposilite /var/log/reposilite
+  fi
 
   exec runuser -u reposilite -- \
     java \
