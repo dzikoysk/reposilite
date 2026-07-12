8b2c078ba865e94fb042d905dadebba1bbd74972
randomusert <eetupajamaki@gmail.com>
2026-07-12 14:15:24 +0300

    Fix link for installation not going to the propper place
    if you are already in /guide it will go to /guide/guide/general for example.

--- reposilite-site/components/layout/Footer.js
+++ reposilite-site/components/layout/Footer.js
@@ -8,7 +8,7 @@ const link = (title, url) =>
 
 const guideLinks = [
   link('Getting Started', '/guide/about'),
-  link('Installation', 'guide/general'),
+  link('Installation', '/guide/general'),
   link('Plugins', '/plugin'),
   link('Developer API', '/guide/sources'),
 ]
